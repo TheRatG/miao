a686888c324ac5655e43d0821783eb4f730bd750
Vladimir <TheRatW@gmail.com>
2012-12-26 15:38:49 +0300

    hot fix

--- modules/Office/classes/Exception.class.php
+++ modules/Office/classes/Exception.class.php
@@ -15,10 +15,6 @@ class Miao_Office_Exception extends Exception
 {
 	public function __construct( $message, $code = 0, Exception $previous = NULL )
 	{
-		if ( !headers_sent() )
-		{
-			header( "HTTP/1.0 404 Not Found" );
-		}
 		parent::__construct( $message, $code, $previous );
 	}
 }
